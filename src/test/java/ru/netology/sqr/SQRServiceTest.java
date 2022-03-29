d77141d981014a36534a9d2cedb26fd25c7b8aef
firuz kholmatov <firuzkholmatov@yandex.ru>
2022-03-29 13:22:22 +0300

    files updated

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -9,8 +9,9 @@ import static org.junit.jupiter.api.Assertions.*;
 class SQRServiceTest {
     @ParameterizedTest
     @CsvSource(value = {"'range200300',3,200,300",
-            "'range300400',2,300,400",
-            "'range400500',2,400,500"})
+            "'range300400',3,300,400",
+            "'range400500',3,400,500",
+            "'range400500',4,600,800"})
 
     public void shouldCountCalc(String testName, int expected, int min, int max) {
         SQRService sqrService = new SQRService();
